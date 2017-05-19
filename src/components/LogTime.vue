958d44a29345d36054023523bd7cd6ee5291cffe
Jam <pjdsilvestre@tmj.ph>
2017-05-19 16:43:42 +0800

    updated logtime

--- src/components/LogTime.vue
+++ src/components/LogTime.vue
@@ -3,7 +3,16 @@
 </template>
 
 <script>
+// import { mapActions } from 'vuex'
 export default {
+  data () {
+
+  },
+  methods: {
+    addTimeEntry: function (data) {
+      this.$store.commit('addTimeEntry', data)
+    }
+  }
 }
 </script>
 
